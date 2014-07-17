1698ffc7e9f7c03b8cad873d60ae89a358225124
U-BROADCOM\asadali <asadali@LTSJC-ASADALI.corp.ad.broadcom.com>
2014-07-16 17:46:19 -0700

    first app

---
+++ wicedhack.mk
@@ -0,0 +1,21 @@
+#
+# Copyright 2014, Broadcom Corporation
+# All Rights Reserved.
+#
+# This is UNPUBLISHED PROPRIETARY SOURCE CODE of Broadcom Corporation;
+# the contents of this file may not be disclosed to third parties, copied
+# or duplicated in any form, in whole or in part, without the prior
+# written permission of Broadcom Corporation.
+#
+
+NAME := WICED_HACK
+
+$(NAME)_SOURCES := wicedhack.c
+
+$(NAME)_COMPONENTS := daemons/gedday
+
+$(NAME)_RESOURCES  := apps/wicedhack/top_web_page_top.html \
+                      images/brcmlogo.png \
+                      images/brcmlogo_line.png \
+                      images/favicon.ico
+GLOBAL_DEFINES :=
