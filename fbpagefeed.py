931bebb97e6fd1d8fd80c4f3c41334f501336e6f
U-BROADCOM\asadali <asadali@LTSJC-ASADALI.corp.ad.broadcom.com>
2014-07-17 00:48:38 -0700

    working python stuff

--- fbpagefeed.py
+++ fbpagefeed.py
@@ -1,7 +1,7 @@
 import requests
 import json
-ACCESS_TOKEN = "CAACEdEose0cBAEqchKMZBolZAN9ADiWpoyjHQu3vQHhrcimPMnKONA7LYZCO6NJsnFqer1CWGqIzS6DXZBnnbVUa6oNLCVHdpdnoGANHJ7bO0a74O21ptngmPh1S9TTLrsADmb5tzzr7txWqagEZCpygseCwWzZCHiMA3l7DRP50ZCV6eHiJZA6l28oiXhPH7cNTduWIOon25mQuNo1UA9LT4IguOOlcZAtEZD"
-
+#ACCESS_TOKEN = "CAACEdEose0cBAEqchKMZBolZAN9ADiWpoyjHQu3vQHhrcimPMnKONA7LYZCO6NJsnFqer1CWGqIzS6DXZBnnbVUa6oNLCVHdpdnoGANHJ7bO0a74O21ptngmPh1S9TTLrsADmb5tzzr7txWqagEZCpygseCwWzZCHiMA3l7DRP50ZCV6eHiJZA6l28oiXhPH7cNTduWIOon25mQuNo1UA9LT4IguOOlcZAtEZD"
+ACCESS_TOKEN = "CAAKbXnsGZA3gBAHTlJE5P1SoUapiVs4zLtgkfenbpHRUxOQNpDeUWwltZBFNwSnK3qxBZBZBdbQEtSYMrwQYAip9fppI5P0IdQp5QTxkZAEeZAezwOLkZBW7jooU2ZAYpxsYhKmsTzYg0Uyq5dInQiKVYnpRirP4kIUjHNK4PL9RIJ1QlZBdD1gOleSPqUjQpNI5qjd0vI09EZCUDZAqWMVOJXWNgscb7FkecoZD"
 query_statement = {"q1": "SELECT message FROM stream WHERE source_id = me()"}
 query_json = json.dumps(query_statement)
 
@@ -19,9 +19,14 @@ if not ("200" in str(query)):
     print "Connection Issues!!"
 
 result_set = query.json()
+# print result_set
+# print json.dumps(result_set, indent = 4)
+#print result_set["data"]
 #print result_set["data"]
 for post in result_set["data"]:
-	print post["message"]
-print result_set
+	#print post
+	if 'message' in post:
+		print post['message']
+# print result_set
 #print result_set["data"][0]["fql_result_set"][0]["message"]
 #print json.dumps(result_set)
